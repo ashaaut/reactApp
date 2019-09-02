8c0756c7e4cac00a6e292ac4d2b63015abbb7693
ashaaut <ashaautade1998@gmail.com>
2019-09-02 05:24:57 -0700

    initial application setup

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import './App.css';
+import Student from './student';
+
+class App extends Component{
+  render(){
+    return(
+      <div className="App">
+        <h1>hello world</h1>
+        <Student/>
+      </div>
+    )
+  }
+}
+
+
+export default App;
---
+++ src/student.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './student.css';
+ const Student=()=>{
+    return(
+        <div className="Student">
+          <p>my name is asha.I am student of GCEK.</p>
+        </div>
+    );
+ }
+
+export default Student;
